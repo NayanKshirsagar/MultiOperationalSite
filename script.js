f35948c5b4ee715b35f49194226640f8471e797d
NayanKshirsagar <nayankshirsagar0101@gmail.com>
2024-09-07 04:03:19 +0530

    my UI developed site

--- script.js
+++ script.js
@@ -1,65 +1,108 @@
-// scripts.js
-
-function calculatePercentage() {
-  const marksObtained = parseFloat(document.getElementById('marksObtained1').value);
-  const totalMarks = parseFloat(document.getElementById('totalMarks1').value);
-  const percentage = (marksObtained / totalMarks) * 100;
-  document.getElementById('percentageResult').innerText = 'Percentage: ' + percentage.toFixed(2) + '%';
+// Percentage Calculator
+function percentage_1() {
+  let percent = document.getElementById("percent").value;
+  let num = document.getElementById("num").value;
+  document.getElementById("value1").value = (num / 100) * percent;
 }
 
-function calculateOverallPercentage() {
-  const subjects = document.getElementById('subjects').value.split(',');
-  let totalMarks = 0;
-  let obtainedMarks = 0;
-  subjects.forEach(subject => {
-      const [obt, total] = subject.split('/').map(Number);
-      obtainedMarks += obt;
-      totalMarks += total;
-  });
-  const overallPercentage = (obtainedMarks / totalMarks) * 100;
-  document.getElementById('overallPercentageResult').innerText = 'Overall Percentage: ' + overallPercentage.toFixed(2) + '%';
-}
-
+// CGPA to Percentage Converter
 function cgpaToPercentage() {
-  const cgpa = parseFloat(document.getElementById('cgpa').value);
-  const percentage = (cgpa - 0.75) * 10;
-  document.getElementById('cgpaResult').innerText = 'Percentage: ' + percentage.toFixed(2) + '%';
+  let cgpa = document.getElementById("cgpa").value;
+  let scale = document.getElementById("cgpa_scale").value;
+  let percentage = scale == 10 ? cgpa * 9.5 : (cgpa / 6) * 100;
+  document.getElementById("cgpa_result").value = percentage + "%";
 }
 
-function percentageToCGPA() {
-  const percentage = parseFloat(document.getElementById('percentage').value);
-  const cgpa = (percentage / 10) + 0.75;
-  document.getElementById('percentageCGPAResult').innerText = 'CGPA: ' + cgpa.toFixed(2);
+// Percentage to CGPA Converter
+function percentageToCgpa() {
+  let percentage = document.getElementById("percentage").value;
+  let scale = document.getElementById("percentage_scale").value;
+  let cgpa = scale == 10 ? percentage / 9.5 : (percentage * 6) / 100;
+  document.getElementById("cgpa_result_from_percentage").value = cgpa.toFixed(2);
 }
 
+// Simple Calculator
 function simpleCalculator() {
-  const num1 = parseFloat(document.getElementById('num1').value);
-  const num2 = parseFloat(document.getElementById('num2').value);
-  const operator = document.getElementById('operator').value;
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let operation = document.getElementById("operation").value;
   let result;
-  switch (operator) {
-      case '+':
-          result = num1 + num2;
-          break;
-      case '-':
-          result = num1 - num2;
-          break;
-      case '*':
-          result = num1 * num2;
-          break;
-      case '/':
-          result = num1 / num2;
-          break;
-      default:
-          result = 'Invalid operation'; 
+  switch (operation) {
+      case '+': result = num1 + num2; break;
+      case '-': result = num1 - num2; break;
+      case '*': result = num1 * num2; break;
+      case '/': result = num1 / num2; break;
   }
-  document.getElementById('simpleCalculatorResult').innerText = 'Result: ' + result;
+  document.getElementById("simple_calc_result").value = result;
 }
 
+// Interest Calculator
 function calculateInterest() {
-  const principal = parseFloat(document.getElementById('principal').value);
-  const rate = parseFloat(document.getElementById('rate').value) / 100;
-  const time = parseFloat(document.getElementById('time').value);
-  const amount = principal * Math.pow(1 + rate, time);
-  document.getElementById('interestResult').innerText = 'Amount: ' + amount.toFixed(2);
+  let principal = parseFloat(document.getElementById("principal").value);
+  let rate = parseFloat(document.getElementById("rate").value);
+  let time = parseFloat(document.getElementById("time").value);
+  let interest = (principal * rate * time) / 100;
+  document.getElementById("interest_result").value = interest;
+}
+
+// Binary Calculator
+function binaryCalculator() {
+  let binary1 = document.getElementById("binary1").value;
+  let binary2 = document.getElementById("binary2").value;
+  let operation = document.getElementById("binary_operation").value;
+  let num1 = parseInt(binary1, 2);
+  let num2 = parseInt(binary2, 2);
+  let result;
+  if (operation === 'add') {
+      result = (num1 + num2).toString(2);
+  } else if (operation === 'subtract') {
+      result = (num1 - num2).toString(2);
+  }
+  document.getElementById("binary_result").value = result;
+}
+
+// Marks Obtained Percentage Calculator
+function marksPercentage() {
+  let marksObtained = document.getElementById("marks_obtained").value;
+  let totalMarks = document.getElementById("total_marks").value;
+  let percentage = (marksObtained / totalMarks) * 100;
+  document.getElementById("marks_percentage_result").value = percentage.toFixed(2) + "%";
+}
+
+// Resets for each calculator
+function resetPercentageCalculator() {
+  document.getElementById('percent').value = '';
+  document.getElementById('num').value = '';
+  document.getElementById('value1').value = '';
+}
+
+function resetMarksPercentageCalculator() {
+  document.getElementById('marks_obtained').value = '';
+  document.getElementById('total_marks').value = '';
+  document.getElementById('marks_percentage_result').value = '';
+}
+
+function resetCgpaToPercentageCalculator() {
+  document.getElementById('cgpa').value = '';
+  document.getElementById('cgpa_scale').selectedIndex = 0;
+  document.getElementById('cgpa_result').value = '';
+}
+
+function resetPercentageToCgpaCalculator() {
+  document.getElementById('percentage').value = '';
+  document.getElementById('percentage_scale').selectedIndex = 0;
+  document.getElementById('cgpa_result_from_percentage').value = '';
+}
+
+function resetSimpleCalculator() {
+  document.getElementById('num1').value = '';
+  document.getElementById('num2').value = '';
+  document.getElementById('operation').selectedIndex = 0;
+  document.getElementById('simple_calc_result').value = '';
+}
+
+function resetBinaryCalculator() {
+  document.getElementById('binary1').value = '';
+  document.getElementById('binary2').value = '';
+  document.getElementById('binary_result').value = '';
 }
